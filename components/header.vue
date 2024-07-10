64b906b04fd79e7c3c29d28bc10833725ee8374c
Rowland <rowland.h.mosbergen@gmail.com>
2024-07-10 12:54:03 +1000

    Adding header

---
+++ components/header.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="header">
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <NuxtLink to="/projects" class="navbar-item">
+        </NuxtLink>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <NuxtLink to="/projects" class="navbar-item">Home</NuxtLink>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.header {
+  background-color: #333;
+  color: white;
+  padding: 1rem;
+}
+
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.navbar-brand .navbar-item img {
+  max-height: 50px;
+}
+
+.navbar-menu .navbar-start .navbar-item {
+  margin-right: 1rem;
+  color: white;
+  text-decoration: none;
+}
+
+.navbar-menu .navbar-start .navbar-item:hover {
+  text-decoration: underline;
+}
+</style>
+
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Header />
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+import Header from '~/components/Header.vue';
+</script>
+
+<style scoped>
+/* You can add additional styles here if needed */
+</style>
+
